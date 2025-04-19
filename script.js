575b2b2a01ef44dda99ca5ee8e095ebd52cae7be
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-19 21:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your code here
+let images = document.querySelectorAll(".image");
+
+images.forEach((img) => {
+    img.addEventListener("dragstart", function(event) {
+        event.dataTransfer.setData("imageId", event.target.id);
+        event.target.classList.add("selected");
+    });
+
+    img.addEventListener("dragend", function(event) {
+        event.target.classList.remove("selected");
+    });
+});
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+
+    var draggedId = event.dataTransfer.getData("imageId");
+    var draggedImg = document.getElementById(draggedId);
+
+   
+    var targetImg = event.target.classList.contains("image") ? event.target : event.target.closest(".image");
+
+   
+    if (targetImg && targetImg.id !== draggedImg.id) {
+        
+        let draggedStyle = window.getComputedStyle(draggedImg).backgroundImage;
+        let targetStyle = window.getComputedStyle(targetImg).backgroundImage;
+
+        targetImg.style.backgroundImage = draggedStyle;
+        draggedImg.style.backgroundImage = targetStyle;
+    }
+});
